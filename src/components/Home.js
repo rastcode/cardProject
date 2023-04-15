7b41f748c74bca070010d09225c648a7759c6750
ali <nematiali1377@gmail.com>
2023-04-15 17:27:54 -0300

    v 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
           <div className='header'>
               <h3 className="h4slim">Reliable, efficient delivery</h3>
               <h3 className="h4bold">Powered by Technology</h3>
-              <p className="pslim">Our Artificial Intelligence powered tools use millions of project data <br/>points to ensure that your project is successful</p>
+              <p className="pslim">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
           </div>
          <Cards />
 
